22ca55729251751656b130fb4e3db552876c8142
Patrick Ramser <pramser@Patricks-MacBook-Pro-2.local>
2020-05-20 18:19:11 -0400

    Test out a 16 x 16 board

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -4,20 +4,17 @@ import { Link } from "react-router-dom";
 export default function Menu() {
   return (
     <div className="Menu">
-      {/* <Link className="menuLink" to="/game/1">
-          Square 1 x 1
-        </Link> */}
       <Link className="menuLink" to="/game/2">
         Square 2 x 2
       </Link>
-      {/* <Link className="menuLink" to="/game/3">
-          Square 3 x 3
-        </Link> */}
       <Link className="menuLink" to="/game/4">
         Square 4 x 4
       </Link>
       <Link className="menuLink" to="/game/9">
         Square 9 x 9
+      </Link>
+      <Link className="menuLink" to="/game/16">
+        Square 16 x 16
       </Link>
     </div>
   );
