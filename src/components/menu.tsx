d23213d808fa79f00753bafd1eda8d1e70c1e1b6
Patrick Ramser <pramser@Patricks-MacBook-Pro-2.local>
2020-05-21 01:59:28 -0400

    Cut down the links in the menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 export default function Menu() {
   return (
     <div className="Menu">
-      <Link className="menuLink" to="/game/2">
-        Square 2 x 2
-      </Link>
-      <Link className="menuLink" to="/game/4">
-        Square 4 x 4
-      </Link>
-      <Link className="menuLink" to="/game/9">
-        Square 9 x 9
-      </Link>
-      <Link className="menuLink" to="/game/16">
-        Square 16 x 16
-      </Link>
+      <GameLink size={2}>Square 2 x 2</GameLink>
+      <GameLink size={4}>Square 4 x 4</GameLink>
+      <GameLink size={9}>Square 9 x 9</GameLink>
+      <GameLink size={16}>Square 16 x 16</GameLink>
     </div>
   );
 }
+
+const GameLink = (props: { size: number; children: any }) => {
+  return (
+    <Link className="menuLink" to={`/game/${props.size}`}>
+      {props.children}
+    </Link>
+  );
+};
